9d64816ba6b331bb6247724514a5bcf9635a90cf
Ingrid <ingridnorton1@gmail.com>
2018-06-15 17:17:31 +1000

    Initial commit

---
+++ Assessment1/Scripts/PlatformGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint; 
+	public float distanceBetween;
+
+	private float platformWidth; 
+
+	// Use this for initialization
+	void Start () {
+		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Generates infinite platforms
+		if(transform.position.x < generationPoint.position.x)
+		{
+			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            
+			//Making the infinite platforms 
+
+			Instantiate(thePlatform, transform.position, transform.rotation);
+
+		}
+	}
+}
---
+++ Assessment1/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   public float moveSpeed;
+	public float jumpForce;
+	private Rigidbody2D myRigidbody;
+	public bool grounded;
+	public LayerMask whatIsGround;
+	private Collider2D myCollider;
+	// Use this for initialization
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+		myCollider = GetComponent<BoxCollider2D>() as Collider2D;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        //Check if grounded is true
+		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+
+		//Check if the A key is pressed
+		if (Input.GetKeyDown(KeyCode.A))
+		{if (grounded)
+			{myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+			
+			}
+		}
+	}
+}
